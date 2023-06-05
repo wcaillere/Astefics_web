eb47ec19ba0a1052eead4515e726ddbab3ab7b78
Administrateur <wcaillere.pro@gmail.com>
2023-06-05 09:11:35 +0200

    Ajout des dépendances nécessaires et configuration application.properties

---
+++ src/main/java/com/astefics/astefics_web/AsteficsWebApplication.java
@@ -0,0 +1,13 @@
+package com.astefics.astefics_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteficsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsteficsWebApplication.class, args);
+    }
+
+}
